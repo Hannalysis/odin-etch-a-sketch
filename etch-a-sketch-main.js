47be001a2d71a75e28a583c5e3a5059cec36129c
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-11-14 20:21:37 +0000

    Improved grid generation, added mouseover trails
    Grid generation - new nested loop - 4 boxes in one row | loop for
    mouseover colour change.

--- etch-a-sketch-main.js
+++ etch-a-sketch-main.js
@@ -1,7 +1,23 @@
 const container = document.querySelector("#container");
 
-for (let i = 1; i < 17; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", `box`);
-    container.appendChild(div);
+
+for (let i = 1; i < 5; i++) {
+    /* Create the row*/
+    const row = document.createElement("div");
+    row.setAttribute("class", `row`);
+    container.appendChild(row);
+    for (let i = 1; i < 5; i++){
+        /* Create the boxes for the row*/
+        const div = document.createElement("div");
+        div.setAttribute("class", `box`);
+        row.appendChild(div);
+    }
+}
+
+const boxes = document.getElementsByClassName("box");
+
+for (box of boxes) {
+    box.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'greenyellow';
+      });
 }
