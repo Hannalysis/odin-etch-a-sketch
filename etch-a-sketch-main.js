9d5781dff4cdeecdddd532c1d9906a498a1e2f5a
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-11-15 21:29:14 +0000

    Button prompt to generate new Grid
    New functions to support the grid and execution inside the button.
    The trail effect is now situated inside its own function.

--- etch-a-sketch-main.js
+++ etch-a-sketch-main.js
@@ -15,9 +15,47 @@ for (let i = 1; i < 5; i++) {
 }
 
 const boxes = document.getElementsByClassName("box");
+function boxTrail(){
+    for (box of boxes) {
+        box.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'greenyellow';
+        });
+    }
+}
 
-for (box of boxes) {
-    box.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'greenyellow';
-      });
+boxTrail();
+
+// Button
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let gridNum = prompt("Grid size request: n x n. n = ");
+    clearGrid();
+    newGrid(gridNum);
+});
+
+function newGrid(number) {
+    number ++;
+    for (let i = 1; i < number; i++) {
+        /* Create the row*/
+        const row = document.createElement("div");
+        row.setAttribute("class", `row`);
+        container.appendChild(row);
+        for (let i = 1; i < number; i++){
+            /* Create the boxes for the row*/
+            const div = document.createElement("div");
+            div.setAttribute("class", `box`);
+            row.appendChild(div);
+        }
+    }
+    boxTrail();
+}
+
+function clearGrid () {
+    const elements = document.getElementsByClassName('row');
+    const elementLength = elements.length;
+    for (let i = 1; i <= elementLength; i++){
+        console.log(elements);
+        elements[0].remove();
+    }
 }
